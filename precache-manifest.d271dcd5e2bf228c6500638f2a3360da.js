4e4e7584ee01faacfc2787c0114b5e3d6eaef704
siva <byteshiva@gmail.com>
2018-06-16 01:11:56 +0530

    fix for url path, changed to /pathname from pathname

---
+++ precache-manifest.d271dcd5e2bf228c6500638f2a3360da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f061f83012f8c2ddafab",
+    "url": "/js/chunk-vendors.f061f830.js"
+  },
+  {
+    "revision": "4bc88c02c9bf37f38f61",
+    "url": "/js/app.4bc88c02.js"
+  },
+  {
+    "revision": "1d67346759ae785fafb2a23ac29b7e86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f061f83012f8c2ddafab",
+    "url": "/css/chunk-vendors.67a4a2c2.css"
+  },
+  {
+    "revision": "4bc88c02c9bf37f38f61",
+    "url": "/css/app.1926c67e.css"
+  }
+];
