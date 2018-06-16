0aa04f3f12f367c2d67ef72aa1758d83be508512
siva <byteshiva@gmail.com>
2018-06-17 01:53:21 +0530

    added store using vuex, updated components with new CreateMeetup..

---
+++ precache-manifest.0596ee7ec8e3e948b5d78f7d3d0271c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52f2646c15462c7af09c",
+    "url": "/js/chunk-vendors.52f2646c.js"
+  },
+  {
+    "revision": "b533e5a1a10a296e1e85",
+    "url": "/js/app.b533e5a1.js"
+  },
+  {
+    "revision": "652db147039f847058bd3e858b0ed091",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52f2646c15462c7af09c",
+    "url": "/css/chunk-vendors.67a4a2c2.css"
+  },
+  {
+    "revision": "b533e5a1a10a296e1e85",
+    "url": "/css/app.a16a7df0.css"
+  }
+];
